594a3c4ac5dc8eb9e0e9e594e0bf34ac26ea130d
skykoradji <rongguiyou@gmail.com>
2019-08-09 10:55:24 +1000

    clean up

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,19 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import { DEAUTHENTICATE, DEFAULT } from '../types';
 
-const appReducer = combineReducers({
+
+const rootReducer = combineReducers({
   authentication: authReducer
 });
 
-const rootReducer = (state, action) => {
-  let currentState = state;
-  const currentAction = action;
-  if (action.type === DEAUTHENTICATE) {
-    currentAction.type = DEFAULT;
-    currentState = {};
-  }
-  return appReducer(currentState, action);
-};
+
 
 export default rootReducer;
